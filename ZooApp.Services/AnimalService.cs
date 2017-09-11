8091bf7e60e7df710fe620248cdf00bebb0d6ec7
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2017-09-11 14:48:31 +0600

    testing 0007

---
+++ ZooApp.Services/AnimalService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooApp.Models;
+using ZooApp.ViewModels;
+
+namespace ZooApp.Services
+{
+    public class AnimalService
+    {
+        //create db object
+        ZooContext db = new ZooContext();
+        //public List<ViewAnimal> GetAnimals()
+        //{
+        //    //fetch db.animal data
+        //    //pulls all row from table in to ram
+        //   // db.Animals.ToList();
+        //    //convert this data into view data
+        //    // return 
+
+        //}
+    }
+}
